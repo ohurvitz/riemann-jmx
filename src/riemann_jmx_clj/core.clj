a4550c353484013a0e446432684d336f7977c360
Omri Hurvitz <omri.hurvitz@twosigma.com>
2015-02-13 12:34:33 -0500

    Added ability to query same queries to multiple jmx hosts

--- src/riemann_jmx_clj/core.clj
+++ src/riemann_jmx_clj/core.clj
@@ -13,33 +13,40 @@
 (let [get-riemann-connection-helper (memoize get-riemann-connection-helper)]
   (defn get-riemann-connection
     ([host]
-     (get-riemann-connection-helper host 5555))
+      (get-riemann-connection-helper host 5555))
     ([host port]
-     (get-riemann-connection-helper host port))))
+      (get-riemann-connection-helper host port))))
+
+(defn run-queries-on-jmx
+  "Takes a jmx connection and queries to run, returning a seq of Riemann events."
+  [jmx queries]
+  (->> (jmx/with-connection jmx
+                            (doall
+                              (for [{:keys [obj attr tags]} queries
+                                    name (jmx/mbean-names obj)
+                                    attr attr]
+                                {:service (str (.getCanonicalName ^javax.management.ObjectName name) \. attr)
+                                 :host    (if (:event_host jmx)
+                                            (:event_host jmx)
+                                            (:host jmx))
+                                 :state   "ok"
+                                 :metric  (jmx/read name attr)
+                                 :tags    tags})))
+       (mapcat (fn [{:keys [service metric] :as event}]
+                 (if (map? metric)
+                   (for [[k v] metric]
+                     (assoc event
+                       :service (str service \. (name k))
+                       :metric v))
+                   [event])))))
 
 (defn run-queries
   "Takes a parsed yaml config and runs the contained queries, returning a seq of Riemann events."
   [yaml]
   (let [{:keys [jmx queries]} yaml]
-    (->> (jmx/with-connection jmx
-           (doall
-             (for [{:keys [obj attr tags]} queries
-                   name (jmx/mbean-names obj)
-                   attr attr]
-               {:service (str (.getCanonicalName ^javax.management.ObjectName name) \. attr)
-                :host (if (:event_host jmx)
-                        (:event_host jmx)
-                        (:host jmx))
-                :state "ok"
-                :metric (jmx/read name attr)
-                :tags tags})))
-         (mapcat (fn [{:keys [service metric] :as event}]
-                   (if (map? metric)
-                     (for [[k v] metric]
-                       (assoc event
-                              :service (str service \.(name k))
-                              :metric v))
-                     [event]))))))
+    (apply concat
+      (for [jmx jmx]
+        (run-queries-on-jmx jmx queries)))))
 
 (defn run-configuration
   "Takes a parsed yaml config, runs the queries, and posts the results to riemann"
@@ -54,11 +61,11 @@
     (riemann/send-events conn events)))
 
 (defn munge-credentials
-  "Takes a parsed yaml config and, if it has jmx username & password,
+  "Takes a jmx host spec and, if it has jmx username & password,
    configures the jmx environment map properly. If only a username or
    password is set, exits with an error"
-  [config]
-  (let [{:keys [host port username password]} (:jmx config)]
+  [host-spec]
+  (let [{:keys [username password]} host-spec]
     (when (and username (not password))
       (println "Provided username but no password.")
       (System/exit 1))
@@ -66,14 +73,22 @@
       (println "Provided password but no username")
       (System/exit 1))
     (if (or username password)
-      (assoc config :jmx {:host host :port port :environment {"jmx.remote.credentials" (into-array String [username password])}})
-      config)))
+      (assoc (dissoc host-spec :username :password) :environment {"jmx.remote.credentials"
+                                                                  (into-array String [username password])})
+      host-spec)))
+
+(defn munge-all-credentials
+  "Takes a parsed yaml config and prepares credentials for all hosts configs.
+  Returnes the yaml with munged hosts, and makes sure hosts is a collection if a single one is specified."
+  [config]
+  (let [hosts (:jmx config) hosts (if (map? hosts) [hosts] hosts)]
+    (assoc config :jmx (map munge-credentials hosts))))
 
 (defn start-config
   "Takes a path to a yaml config, parses it, and runs it in a loop"
   [config]
   (let [yaml (yaml/parse-string (slurp config))
-        munged (munge-credentials yaml)]
+        munged (munge-all-credentials yaml)]
     (pprint munged)
     (future
       (while true
